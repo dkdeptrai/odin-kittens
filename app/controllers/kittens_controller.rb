a92eb61b4707bed11482429c23c65489f8104bac
Mona Lisa <khoa.soft.e@gmail.com>
2023-11-03 15:11:53 +0700

    added Kitten CRUD

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+    render 'edit'    
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = 'Kitten successfully created!'
+      redirect_to @kitten
+    else
+      flash[:error] = 'Silly you, something went wrong!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    if @kitten.destroy
+      flash[:success] = 'Kitten was successfully deleted.'
+      redirect_to kittens_url
+    else
+      flash[:error] = 'Silly you, something went wrong!'
+      redirect_to kittens_url
+    end
+  end
+  
+
+  private
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+end
